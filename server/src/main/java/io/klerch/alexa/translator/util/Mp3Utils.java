510643d30dedad07404534da5a84c2562a230e2b
Lerch, Kay <kay.lerch@immobilienscout24.de>
2016-12-06 21:31:22 +0100

    Make mp3 louder with ffmpeg

--- server/src/main/java/io/klerch/alexa/translator/util/Mp3Utils.java
+++ server/src/main/java/io/klerch/alexa/translator/util/Mp3Utils.java
@@ -10,8 +10,10 @@ import net.bramp.ffmpeg.options.AudioEncodingOptions;
 import net.bramp.ffmpeg.options.EncodingOptions;
 import net.bramp.ffmpeg.options.MainEncodingOptions;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Mp3Utils {
     private static String AUDIO_MP3_CODEC = "libmp3lame";
@@ -24,18 +26,6 @@ public class Mp3Utils {
         // will read out path to executables from environment variables FFMPEG and FFPROBE
         // take care of those variables being set in your system
         final FFmpeg ffmpeg = new FFmpeg();
-
-        System.out.println("Version: " + ffmpeg.version());
-        System.out.println("Path: " + ffmpeg.getPath());
-
-        for (final Codec codec : ffmpeg.codecs()) {
-            System.out.println("Codec: " + codec.getName());
-        }
-
-        for (final Format format : ffmpeg.formats()) {
-            System.out.println("Format: " + format.getName());
-        }
-
         final FFprobe ffprobe = new FFprobe();
         // build a configuration according to what Alexa expects from an MP3 it supports
         // see: https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/speech-synthesis-markup-language-ssml-reference#audio
@@ -53,6 +43,19 @@ public class Mp3Utils {
         final FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
         // Run a one-pass encode
         executor.createJob(builder).run();
+        return new File(mp3Filename);
+    }
+
+    public static File convertUrlToMp3Manual(final String url, final String mp3Filename) throws IOException, InterruptedException {
+        final Process p = Runtime.getRuntime().exec("ffmpeg -i " + url + " -ac 2 -codec:a libmp3lame -b:a 48k -ar 16000 -af volume=12dB " + mp3Filename);
+        p.waitFor();
+
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+        String line = "";
+        while ((line = reader.readLine())!= null) {
+            System.out.println(line);
+        }
         return new File(mp3Filename);
     }
 }
