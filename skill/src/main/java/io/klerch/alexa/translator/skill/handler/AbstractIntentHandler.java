09ec20e2e4f50c91addbfb62e3e1f176a40152e1
kaylerch <dekstar@gmx.de>
2016-12-09 14:45:26 +0100

    Source out functional logic

--- skill/src/main/java/io/klerch/alexa/translator/skill/handler/AbstractIntentHandler.java
+++ skill/src/main/java/io/klerch/alexa/translator/skill/handler/AbstractIntentHandler.java
@@ -2,13 +2,18 @@ package io.klerch.alexa.translator.skill.handler;
 
 import com.amazon.speech.ui.Card;
 import com.amazon.speech.ui.SimpleCard;
+import io.klerch.alexa.state.handler.AWSDynamoStateHandler;
 import io.klerch.alexa.state.utils.AlexaStateException;
 import io.klerch.alexa.tellask.model.AlexaInput;
 import io.klerch.alexa.tellask.model.AlexaOutput;
 import io.klerch.alexa.tellask.schema.AlexaIntentHandler;
 import io.klerch.alexa.tellask.util.AlexaRequestHandlerException;
 import io.klerch.alexa.translator.skill.model.TextToSpeech;
+import io.klerch.alexa.translator.skill.util.GoogleTranslation;
+import io.klerch.alexa.translator.skill.util.TTSPolly;
 import org.apache.log4j.Logger;
+
+import java.util.Optional;
 
 abstract class AbstractIntentHandler implements AlexaIntentHandler {
     private static final Logger log = Logger.getLogger(AbstractIntentHandler.class);
@@ -37,6 +42,33 @@ abstract class AbstractIntentHandler implements AlexaIntentHandler {
         return AlexaOutput.tell("SayTranslate")
                 .withCard(card)
                 .putState(tts)
+                .build();
+    }
+
+    AlexaOutput sayTranslate(final AlexaInput input, final String text, final String lang) {
+        final TTSPolly ttsPolly = new TTSPolly(input.getLocale(), lang);
+
+        // translate term
+        final Optional<String> translated = new GoogleTranslation(input.getLocale()).translate(text, lang);
+
+        if (translated.isPresent()) {
+            // translated term to speech
+            final Optional<TextToSpeech> tts = ttsPolly.textToSpeech(text, translated.get());
+
+            if (tts.isPresent()) {
+                final AWSDynamoStateHandler dynamoHandler = new AWSDynamoStateHandler(input.getSessionStateHandler().getSession());
+                // set language and handler to keep in mind all information related to last translation
+                tts.get().withLanguage(lang).withHandler(dynamoHandler);
+                return sayTranslate(tts.get());
+            } else {
+                log.warn(String.format("Did not get result of text-to-speech for '$1%s'", translated.get()));
+            }
+        } else {
+            log.warn(String.format("Did not get result of translation for '$1%s' from '$2%s' to '$3%s'.", text, input.getLocale(), lang));
+        }
+        return AlexaOutput.tell("SayNoTranslation")
+                .putSlot("text", text)
+                .putSlot("language", lang)
                 .build();
     }
 }
--- skill/src/main/java/io/klerch/alexa/translator/skill/handler/TranslateHandler.java
+++ skill/src/main/java/io/klerch/alexa/translator/skill/handler/TranslateHandler.java
@@ -1,17 +1,11 @@
 package io.klerch.alexa.translator.skill.handler;
 
-import io.klerch.alexa.state.handler.AWSDynamoStateHandler;
 import io.klerch.alexa.state.utils.AlexaStateException;
 import io.klerch.alexa.tellask.model.AlexaInput;
 import io.klerch.alexa.tellask.model.AlexaOutput;
 import io.klerch.alexa.tellask.schema.annotation.AlexaIntentListener;
 import io.klerch.alexa.tellask.util.AlexaRequestHandlerException;
-import io.klerch.alexa.translator.skill.model.TextToSpeech;
-import io.klerch.alexa.translator.skill.util.GoogleTranslation;
-import io.klerch.alexa.translator.skill.util.TTSPolly;
 import org.apache.log4j.Logger;
-
-import java.util.Optional;
 
 @AlexaIntentListener(customIntents = "Translate")
 public class TranslateHandler extends AbstractIntentHandler {
@@ -31,29 +25,6 @@ public class TranslateHandler extends AbstractIntentHandler {
         final String lang = input.getSlotValue("language");
         final String text = sb.toString();
 
-        final TTSPolly ttsPolly = new TTSPolly(input.getLocale(), lang);
-
-        // translate term
-        final Optional<String> translated = new GoogleTranslation(input.getLocale()).translate(text, lang);
-
-        if (translated.isPresent()) {
-            // translated term to speech
-            final Optional<TextToSpeech> tts = ttsPolly.textToSpeech(text, translated.get());
-
-            if (tts.isPresent()) {
-                final AWSDynamoStateHandler dynamoHandler = new AWSDynamoStateHandler(input.getSessionStateHandler().getSession());
-                // set language and handler to keep in mind all information related to last translation
-                tts.get().withLanguage(lang).withHandler(dynamoHandler);
-                return sayTranslate(tts.get());
-            } else {
-                log.warn(String.format("Did not get result of text-to-speech for '$1%s'", translated.get()));
-            }
-        } else {
-            log.warn(String.format("Did not get result of translation for '$1%s' from '$2%s' to '$3%s'.", text, input.getLocale(), lang));
-        }
-        return AlexaOutput.tell("SayNoTranslation")
-                .putSlot("text", text)
-                .putSlot("language", lang)
-                .build();
+        return sayTranslate(input, text, lang);
     }
 }
